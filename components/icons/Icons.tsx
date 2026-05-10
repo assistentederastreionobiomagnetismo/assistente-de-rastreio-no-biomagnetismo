aea88e15df5d21c5cb811883743c80b624760449
assistentederastreionobiomagnetismo <cursobiomagnetismo.goiania@gmail.com>
2026-05-10 19:15:36 -0300

    novo ajuste no deploy
    novo ajuste no deploy

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -181,3 +181,9 @@ export const XIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
   </svg>
 );
+
+export const ChevronLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+  </svg>
+);
